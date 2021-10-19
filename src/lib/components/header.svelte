fd850ea47c61ebd227375fcc8d36cd73a93b5de6
Matheus Andrade <matheusa56@gmail.com>
2021-10-18 23:02:42 -0300

    Add taiwindcss and create header component

---
+++ src/lib/components/header.svelte
@@ -0,0 +1,17 @@
+<header class="w-full p-4 shadow-xl">
+  <div class="container mx-auto flex justify-between items-center">
+    <a href="/">
+      <h1 class="text-3xl font-semibold border p-1 rounded border-black border-4">
+        TheCoolKid
+      </h1>
+    </a>  
+    <nav class="list-none flex justify-between">
+      <li class="ml-4">
+        <a href="/products" class="text-lg">Produtos</a>
+      </li>
+      <li class="ml-4">
+        <a href="/about" class="text-lg">Sobre</a>
+      </li>
+    </nav>
+  </div>
+</header>
